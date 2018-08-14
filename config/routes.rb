5ab7ef66d6f9a470dbf2d78bdd6086e56cea1cb8
Wesley Sprague <wesleysprague@yahoo.com>
2018-08-14 15:24:03 +0100

    created task tracker

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+   get 'tasks', to: 'tasks#index'
+   get 'tasks/:id', to: 'tasks#show', as: :show
+   get 'task/new', to: 'tasks#new'
+   get 'task/edit/:id', to: 'tasks#edit', as: :task
+   post 'tasks', to: 'tasks#create'
+   patch 'task/edit/:id', to: 'tasks#update'
+   delete 'task/edit/:id', to: 'tasks#destroy', as: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
